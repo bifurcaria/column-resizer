e55fc200727b04e6cc898aa0d5b1c2a5995eaec1
carlapazjm <carla.jana@procure.cl>
2023-12-29 15:23:31 -0300

    first commit

---
+++ column-resize/src/components/sampleTable.js
@@ -0,0 +1,49 @@
+import { React, useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import useColumnResizer from '@/hooks/useColumnResizer';
+
+const SampleTable = () => {
+    const [loading, setLoading] = useState(true);
+    const resizedColumnWidths = useColumnResizer(loading);
+
+    useEffect(() => {
+        setLoading(false);
+    }, [])
+
+
+    let columns = [
+        { field: 'id', headerName: 'ID', width: 100 },
+        { field: 'name', headerName: 'Name', width: 200 },
+        { field: 'email', headerName: 'Email', width: 250 },
+        { field: 'age', headerName: 'Age', width: 100 },
+    ];
+    // add classes to each column
+    columns = columns.map((column, index) => {
+        column.cellClassName = `colIndex-${index + 1}`;
+        column.headerClassName = `colIndex-${index + 1}`;
+        return column;
+    });
+
+    const rows = [
+        { id: 1, name: 'John Doe', email: 'john.doe@example.com', age: 25 },
+        { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com', age: 30 },
+        { id: 3, name: 'Bob Johnson', email: 'bob.johnson@example.com', age: 35 },
+    ];
+
+    return (
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={5}
+                sx={{
+                    '.MuiDataGrid-iconSeparator': {
+                        '& path:hover': { cursor: 'col-resize', strokeWidth: '3px' },
+                    },
+                    ...{ ...resizedColumnWidths }
+                }} />
+        </div>
+    );
+};
+
+export default SampleTable;
